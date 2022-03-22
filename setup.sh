a8f941c5a9209a8cca4372f0045260153aefda3f
Brad Sokol <bsokol@gmail.com>
2022-03-22 10:02:10 -0400

    No need to remove credential helper config during setup

--- setup.sh
+++ setup.sh
@@ -97,8 +97,6 @@ if [ $SPIN ]; then
   # sudo update-alternatives --remove vi /usr/bin/nvim
   # sudo update-alternatives --remove vim /usr/bin/nvim
 
-  git config --global --unset-all credential.helper
-
   for dir in ~/src/github.com/Shopify/*/ ; do
     cd $dir
     git shopify
