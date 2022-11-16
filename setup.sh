01d19d82a209fdd0240bd58e8a1c47cdd1c21692
Brad Sokol <bsokol@gmail.com>
2022-11-16 08:59:26 -0500

    Install neovim plugins during setup

--- setup.sh
+++ setup.sh
@@ -104,7 +104,7 @@ fi
 # vim-plug for Neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-vim +PlugInstall +qall | tee /tmp/plug_install.txt
+nvim +PlugInstall +qall | tee /tmp/plug_install.txt
 
 if [ $SPIN ]; then
   # sudo update-alternatives --remove vi /usr/bin/nvim
