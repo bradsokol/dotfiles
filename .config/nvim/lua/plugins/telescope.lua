3b52de61890c1bed8ba7c4546ff0c2c8b3d58934
Brad Sokol <bsokol@gmail.com>
2024-03-06 13:22:28 -0500

    Use fzf with telescope in Neovim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,15 @@
 return {
   'nvim-telescope/telescope.nvim', branch = '0.1.x',
-  dependencies = { { 'nvim-lua/plenary.nvim' } },
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
   keys = {
     {'<C-a>', '<cmd>Telescope live_grep<cr>', desc = 'Live grep'},
     {'<C-p>', '<cmd>Telescope git_files<cr>', desc = 'Git files'},
