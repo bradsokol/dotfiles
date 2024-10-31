9c842731f6c1cedd3a296dcffe69fe2378058140
Brad Sokol <bsokol@gmail.com>
2024-10-31 13:02:35 -0400

    Copy options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,65 +1,87 @@
 -- [[ Setting options ]]
--- See `:help vim.opt`
+-- See `:help opt`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+local opt = vim.opt
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+-- Whitespace handling
 
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+opt.autoindent = true
+opt.tabstop = 2
+opt.shiftround = true
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.wrap = true
+opt.breakindent = true
+
+-- Editing
+
+opt.formatoptions:append 'cjqrn1'
+opt.showmatch = true
+opt.textwidth = 120
+
+-- Visual
+
+opt.colorcolumn = '120'
+opt.cursorline = true
+opt.laststatus = 2
+opt.number = true
+opt.relativenumber = true
+opt.showcmd = true
+opt.showmode = false
+opt.signcolumn = 'yes'
+opt.termguicolors = true
+
+-- Search
+
+opt.incsearch = true
+opt.hlsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.gdefault = true
+
+-- Splits
+
+opt.splitbelow = true
+opt.splitright = true
+
+-- Other
+
+opt.confirm = true
+opt.mouse = 'a'
+opt.encoding = 'utf-8'
+opt.scrolloff = 3
+opt.wildmenu = true
+opt.wildignore:append '*/.git/*,*/.hg/*,*.pyc'
+opt.visualbell = true
+opt.backspace:append 'indent,eol,start'
+opt.completeopt:append 'menuone,longest,preview'
+opt.undofile = true
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+  opt.clipboard = 'unnamedplus'
 end)
 
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
+-- Decrease swap file update time
+opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+opt.timeoutlen = 300
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+opt.inccommand = 'split'
 
 -- vim: ts=2 sts=2 sw=2 et
