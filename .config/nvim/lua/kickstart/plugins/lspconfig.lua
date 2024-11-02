9f7e3763d7dfa17db93771c22dd5ea1613dcc86c
Brad Sokol <bsokol@gmail.com>
2024-11-02 11:25:31 -0400

    Use Mason to ensure installation of formatters and linters

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -210,6 +210,10 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'black',
+        'isort',
+        'prettier',
+        'pylint',
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
