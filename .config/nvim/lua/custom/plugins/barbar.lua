deb210ce8ba37ee0d615c6fc6dd029ada3e6d97c
Brad Sokol <bsokol@gmail.com>
2024-11-12 14:25:48 -0500

    Disable barbar in favour of LuaLine

--- .config/nvim/lua/custom/plugins/barbar.lua
+++ .config/nvim/lua/custom/plugins/barbar.lua
@@ -1,23 +1,24 @@
-return {
-  {
-    'romgrk/barbar.nvim',
-    dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
-    init = function()
-      vim.g.barbar_auto_setup = false
-    end,
-    opts = {
-      icons = {
-        buffer_index = true,
-      },
-      gitsigns = {
-        added = { enabled = true, icon = '+' },
-        changed = { enabled = true, icon = '~' },
-        deleted = { enabled = true, icon = '-' },
-      },
-    },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
-  },
-}
+return {}
+-- return {
+--   {
+--     'romgrk/barbar.nvim',
+--     dependencies = {
+--       'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+--       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+--     },
+--     init = function()
+--       vim.g.barbar_auto_setup = false
+--     end,
+--     opts = {
+--       icons = {
+--         buffer_index = true,
+--       },
+--       gitsigns = {
+--         added = { enabled = true, icon = '+' },
+--         changed = { enabled = true, icon = '~' },
+--         deleted = { enabled = true, icon = '-' },
+--       },
+--     },
+--     version = '^1.0.0', -- optional: only update when a new 1.x version is released
+--   },
+-- }
