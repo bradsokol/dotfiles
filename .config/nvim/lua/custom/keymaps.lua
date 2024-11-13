2c61670a2beed36ee80114568e9cb14c0a980631
Brad Sokol <bsokol@gmail.com>
2024-11-12 21:13:19 -0500

    Show diagnostics when cursor moves to line

--- .config/nvim/lua/custom/keymaps.lua
+++ .config/nvim/lua/custom/keymaps.lua
@@ -50,3 +50,25 @@ end, { desc = 'Show the definition of current symbol' })
 vim.keymap.set('n', 'gD', function()
   vim.lsp.buf.declaration()
 end, { desc = 'Declaration of current symbol' })
+
+vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+  pattern = '*',
+  callback = function()
+    for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+      if vim.api.nvim_win_get_config(winid).zindex then
+        return
+      end
+    end
+    vim.diagnostic.open_float {
+      scope = 'cursor',
+      focusable = false,
+      close_events = {
+        'CursorMoved',
+        'CursorMovedI',
+        'BufHidden',
+        'InsertCharPre',
+        'WinLeave',
+      },
+    }
+  end,
+})
