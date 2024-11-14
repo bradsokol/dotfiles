c21b0b7f3055d30010e868e34747ce95383c935a
Brad Sokol <bsokol@gmail.com>
2024-11-13 21:33:19 -0500

    Better symbols for diagnostics

--- .config/nvim/lua/kickstart/polish.lua
+++ .config/nvim/lua/kickstart/polish.lua
@@ -12,3 +12,9 @@ require('ibl').setup {
   scope = { highlight = scope },
   indent = { highlight = indent },
 }
+
+local signs = { Error = '󰅚 ', Warn = '󰀪 ', Hint = '󰌶 ', Info = ' ' }
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
