11fdf8604ca7da35f2624c119ed3bae4d8ef5ebf
Brad Sokol <bsokol@gmail.com>
2025-02-20 16:26:15 -0500

    Disable Ruby linting. It's handled elsewhere

--- .config/nvim/lua/kickstart/plugins/lint.lua
+++ .config/nvim/lua/kickstart/plugins/lint.lua
@@ -5,14 +5,14 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
       lint.linters_by_ft = lint.linters_by_ft or {}
+      lint.linters_by_ft['markdown'] = { 'markdownlint' }
       lint.linters_by_ft['python'] = { 'pylint' }
+      lint.linters_by_ft['ruby'] = nil
+      lint.linters_by_ft['swift'] = { 'swiftlint' }
       --
       -- However, note that this will enable a set of default linters,
       -- which will cause errors unless these tools are available:
