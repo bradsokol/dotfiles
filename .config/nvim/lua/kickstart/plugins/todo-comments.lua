996f09169aeef01f5e29ffa0a0595538c99692b5
Brad Sokol <bsokol@gmail.com>
2025-02-20 20:12:33 -0500

    Additional config for todo-comments Neovim plugin
    - Show symbols
    - Add keymaps for jumps

--- .config/nvim/lua/kickstart/plugins/todo-comments.lua
+++ .config/nvim/lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,22 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('todo-comments').setup {
+        signs = true,
+      }
+
+      vim.keymap.set('n', ']t', function()
+        require('todo-comments').jump_next()
+      end, { desc = 'Next todo comment' })
+
+      vim.keymap.set('n', '[t', function()
+        require('todo-comments').jump_prev()
+      end, { desc = 'Previous todo comment' })
+    end,
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
