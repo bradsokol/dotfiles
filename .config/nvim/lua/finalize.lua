67caf52c2ac6509143093f08869fe485422e46e6
Brad Sokol <bsokol@gmail.com>
2025-11-17 11:21:20 -0500

    Fix deprecated use of `sign_define`

--- .config/nvim/lua/finalize.lua
+++ .config/nvim/lua/finalize.lua
@@ -15,8 +15,19 @@ require('ibl').setup {
 }
 
 -- Use diagnostic symbols in the sign column (gutter)
-local signs = { Error = '󰅚 ', Warn = '󰀪 ', Hint = '󰌶 ', Info = ' ' }
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config {
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+      [vim.diagnostic.severity.INFO] = ' ',
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = 'Error',
+      [vim.diagnostic.severity.WARN] = 'Warn',
+      [vim.diagnostic.severity.HINT] = 'Hint',
+      [vim.diagnostic.severity.INFO] = 'Info',
+    },
+  },
+}
