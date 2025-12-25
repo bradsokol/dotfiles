15c0c813499f5b8192b1bb87b11cb8430bd87334
Brad Sokol <bsokol@gmail.com>
2025-12-25 08:56:47 -0500

    Show Trouble quickfix window instead of Xcode logs on errors

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -47,5 +47,26 @@ return {
       severity = vim.diagnostic.severity.ERROR,
       cycle_results = false,
     }
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = { 'XcodebuildBuildFinished', 'XcodebuildTestsFinished' },
+      callback = function(event)
+        if event.data.cancelled then
+          return
+        end
+
+        if event.data.success then
+          require('trouble').close()
+        elseif not event.data.failedCount or event.data.failedCount > 0 then
+          if next(vim.fn.getqflist()) then
+            require('trouble').open 'quickfix'
+          else
+            require('trouble').close()
+          end
+
+          require('trouble').refresh()
+        end
+      end,
+    })
   end,
 }
--- .config/nvim/lua/plugins/xcodebuild.lua
+++ .config/nvim/lua/plugins/xcodebuild.lua
@@ -17,7 +17,29 @@ return {
     'nvim-treesitter/nvim-treesitter', -- (optional) for Quick tests support (required Swift parser)
   },
   config = function()
-    require('xcodebuild').setup {}
+    require('xcodebuild').setup {
+      logs = { -- build & test logs
+        auto_open_on_success_tests = false, -- open logs when tests succeeded
+        auto_open_on_failed_tests = false, -- open logs when tests failed
+        auto_open_on_success_build = false, -- open logs when build succeeded
+        auto_open_on_failed_build = false, -- open logs when build failed
+        auto_close_on_app_launch = false, -- close logs when app is launched
+        auto_close_on_success_build = false, -- close logs when build succeeded (only if auto_open_on_success_build=false)
+        auto_focus = true, -- focus logs buffer when opened
+        filetype = '', -- file type set for buffer with logs
+        open_command = 'silent botright 20split {path}', -- command used to open logs panel. You must use {path} variable to load the log file
+        logs_formatter = 'xcbeautify --disable-colored-output --disable-logging', -- command used to format logs, you can use "" to skip formatting
+        only_summary = false, -- if true logs won't be displayed, just xcodebuild.nvim summary
+        live_logs = true, -- if true logs will be updated in real-time
+        show_warnings = true, -- show warnings in logs summary
+        notify = function(message, severity) -- function to show notifications from this module (like "Build Failed")
+          vim.notify(message, severity)
+        end,
+        notify_progress = function(message) -- function to show live progress (like during tests)
+          vim.cmd("echo '" .. message .. "'")
+        end,
+      },
+    }
     vim.keymap.set('n', '<leader>A', '<cmd>XcodebuildPicker<cr>', { desc = 'Show [A]pple (Xcode) Actions' })
     vim.keymap.set('n', '<leader>af', '<cmd>XcodebuildProjectManager<cr>', { desc = 'Show Project Manager Actions' })
 
