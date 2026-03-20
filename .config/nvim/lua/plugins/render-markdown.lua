9fd6f24fd7ec9956662df9a9dc2773cbd1119953
Brad Sokol <bsokol@gmail.com>
2026-03-20 08:41:41 -0400

    Added Render Markdown

---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,7 @@
+-- Render markdown
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+  opts = {},
+}
